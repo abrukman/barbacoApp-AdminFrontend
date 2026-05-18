d3beeb4a0be840655193e275e2b87a8db1c7cbca
Ariel <brukmanster@gmail.com>
2026-05-18 17:47:31 -0300

    se modifica BASE_URL para que pueda funcionar con entorno vite

--- src/api/canciones.js
+++ src/api/canciones.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:5000/api/canciones";
+const BASE_URL =
+  import.meta.env.VITE_API_URL || "http://localhost:5000/api/canciones";
 
 export async function traerCanciones() {
   const res = await fetch(BASE_URL);
